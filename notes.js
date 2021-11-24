14e17be100e68aec5dfa9245a77ea5e5377bc4b5
jerrytifleboyer <53534200+jerrytifleboyer@users.noreply.github.com>
2021-11-24 10:31:26 -0800

    finsihed adding all the categories in a cocktail

--- notes.js
+++ notes.js
@@ -1,9 +1,33 @@
 let spirits = [
   "gin",
-  "whiskey",
   "vodka",
   "rum",
-  "scotch",
-  "bourbon",
   "tequila",
+  "bourbon",
+  "scotch",
+  "whiskey",
+  "rye",
+  "brandy/cognac",
+];
+
+let citrus = ["orange", "lemon", "lime"];
+
+let sweets = [
+  "simple syrup",
+  "sweet/sour mix",
+  "cointreau",
+  "coke",
+  "ginger beer",
+  "tonic",
+];
+
+let others = [
+  "champagne/sparkling wine",
+  "club soda",
+  "vermouth",
+  "aperol/campari",
+  "bitters",
+  "egg white",
+  "mint",
+  "cucumber",
 ];
