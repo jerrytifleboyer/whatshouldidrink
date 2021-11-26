048a5289a4f316add015360d900ff3ada7aad6c1
jerrytifleboyer <53534200+jerrytifleboyer@users.noreply.github.com>
2021-11-25 17:00:17 -0800

    finished combining the ingredient highlight toggle and cocktails they belong

--- script.js
+++ script.js
@@ -2,33 +2,44 @@
 
 const cocktails = {
   "moscow mule": ["vodka", "ginger beer", "lime"],
+  cosmopolitan: ["vodka", "cranberry", "lemon", "cointreau", "simple syrup"],
+  "vodka cranberry": ["vodka", "cranberry"],
+  "lemon drop": ["vodka", "cointreau", "lemon", "simple syrup"],
+
   "gin and tonic": ["gin", "lime", "tonic"],
+  "tom collins": ["gin", "lemon", "simple syrup", "club soda"],
+  gimlet: ["gin", "lime", "simple syrup"],
+  bramble: ["gin", "lemon", "creme de mure"],
+  "gin fizz": ["gin", "lemon", "simple syrup", "egg white", "club soda"],
+  "french 75": ["gin", "lemon", "simple syrup", "champagne"],
+  "south side": ["gin", "lime", "simple syrup", "mint"],
+  "gin mule": ["gin", "lime", "ginger beer"],
+  negroni: ["gin", "aperol", "vermouth"],
+  martini: ["gin", "vermouth"],
+
+  "whiskey sour": ["whiskey", "lemon", "simple syrup", "egg white", "bitters"],
+  "irish mule": ["whiskey", "lime", "ginger beer"],
+  sidecar: ["cognac", "cointreau", "lemon"],
 };
 
 const ingredients = document.querySelectorAll(".ingredient");
 
-// clicking the ingredients toggles them on
 for (let i = 0; i < ingredients.length; i++) {
+  const ingredient = document.querySelector(
+    `.${ingredients[i].textContent.replace(/\s+/g, "")}`
+  );
+  const ingredientClicked = ingredients[i].textContent;
+
   ingredients[i].addEventListener("click", function () {
-    document.querySelector(`.${ingredients[i].textContent}`).style
-      .backgroundColor == ""
-      ? (document.querySelector(
-          `.${ingredients[i].textContent}`
-        ).style.backgroundColor = "green")
-      : (document.querySelector(
-          `.${ingredients[i].textContent}`
-        ).style.backgroundColor = "");
+    if (ingredient.style.backgroundColor == "") {
+      ingredient.style.backgroundColor = "green";
+      for (const cocktail in cocktails) {
+        if (cocktails[cocktail].includes(ingredientClicked)) {
+          console.log(cocktail);
+        }
+      }
+    } else {
+      ingredient.style.backgroundColor = "";
+    }
   });
 }
-
-// //clicking the ingredients displays their respective cocktails
-// for (let i = 0; i < ingredients.length; i++) {
-//   ingredients[i].addEventListener("click", function () {
-//     const ingredientClicked = ingredients[i].textContent;
-//     for (const cocktail in cocktails) {
-//       if (cocktails[cocktail].includes(ingredientClicked)) {
-//         console.log(cocktail);
-//       }
-//     }
-//   });
-// }
---
+++ test.js
@@ -0,0 +1,9 @@
+const object = ["stuff", "egg"];
+
+const shoppingcart = ["egg", "stuff", "monkey"];
+
+if (shoppingcart.includes(object)) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
