a4a3130eec90e463c999b7dc11c8246012f9534c
jerrytifleboyer <53534200+jerrytifleboyer@users.noreply.github.com>
2021-11-28 02:11:44 -0800

    added new cocktails to the list

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 "use strict";
-
-const cocktails = {
-  "moscow mule": ["vodka", "ginger beer", "lime"],
-  cosmopolitan: ["vodka", "cranberry", "lemon", "cointreau", "simple syrup"],
-  "vodka cranberry": ["vodka", "cranberry"],
-  "lemon drop": ["vodka", "cointreau", "lemon", "simple syrup"],
-
-  "gin and tonic": ["gin", "lime", "tonic"],
-  "tom collins": ["gin", "lemon", "simple syrup", "club soda"],
-  gimlet: ["gin", "lime", "simple syrup"],
-  bramble: ["gin", "lemon", "creme de mure"],
-  "gin fizz": ["gin", "lemon", "simple syrup", "egg white", "club soda"],
-  "french 75": ["gin", "lemon", "simple syrup", "champagne"],
-  "south side": ["gin", "lime", "simple syrup", "mint"],
-  "gin mule": ["gin", "lime", "ginger beer"],
-  negroni: ["gin", "aperol", "vermouth"],
-  martini: ["gin", "vermouth"],
-
-  "whiskey sour": ["whiskey", "lemon", "simple syrup", "egg white", "bitters"],
-  "irish mule": ["whiskey", "lime", "ginger beer"],
-  sidecar: ["cognac", "cointreau", "lemon"],
-};
+//this is sorta dumb but the html is reading from the cocktails.js file
+//dictionary is in cocktail.js
 
 const ingredients = document.querySelectorAll(".ingredient");
 
